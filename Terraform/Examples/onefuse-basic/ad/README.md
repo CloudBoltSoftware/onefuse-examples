7bdfc4c3f6a3c095e59e998111d5134665542b0c
Sid Smith <ssmith@sovlabs.com>
2021-03-02 18:47:42 -0500

    updated basic ad example

---
+++ Terraform/Examples/onefuse-basic/ad/README.md
@@ -0,0 +1,46 @@
+# Terraform Basic Examples
+
+The examples contained in this repo are tied to the OneFuse Terraform provider.  For more information or to assistance with the OneFuse Terraform provider or examples please visit the [OneFuse Community](https://onefuse.cloudbolt.io/).
+
+## Environment Variables if you want to use them:
+
+```
+//OneFuse Provider Info
+TF_VAR_onefuse_address
+TF_VAR_onefuse_port
+TF_VAR_onefuse_user
+TF_VAR_onefuse_password
+
+//vSphere Provider Info
+TF_VAR_vsphere_server
+TF_VAR_vsphere_user
+TF_VAR_vsphere_password
+```
+
+## terraform.tfvars example
+
+```
+//OneFuse Provider Info
+onfuse_address = ""
+onefuse_port = ""
+onefuse_user = ""
+onefuse_password = ""
+
+//vSphere Provider Info
+vsphere_server = ""
+vsphere_user = ""
+vsphere_password = ""
+```
+
+## Usage
+
+### /examples/onefuse-module
+
+Usage examples of how to consume OneFuse Terraform Provider
+
+```
+terraform init
+terraform plan -var="hostname=testname" 
+terraform apply --auto-approve -var="name=testname"
+terraform destroy --auto-approve -var="name=testname"
+```
---
+++ Terraform/Examples/onefuse-basic/ad/main.tf
@@ -0,0 +1,42 @@
+ // Comment out the following for Terraform 0.12 to work
+
+ terraform {
+  required_providers {
+    onefuse = {
+      source  = "CloudBoltSoftware/onefuse"
+      version = ">= 1.20.0"
+   }
+  }
+  required_version = ">= 0.13"
+}
+// Comment out the above for Terraform 0.12
+
+ // Inititalize OneFuse Provider
+provider "onefuse" {
+
+  scheme     = var.onefuse_scheme
+  address    = var.onefuse_address
+  port       = var.onefuse_port
+  user       = var.onefuse_user
+  password   = var.onefuse_password
+  verify_ssl = var.onefuse_verify_ssl
+}
+
+// OneFuse Data Source for AD Policy to lookup policy ID
+data "onefuse_ad_policy" "policy" {
+  name = "prod"
+}
+
+// OneFuse Resource for AD Computer Account
+resource "onefuse_microsoft_ad_computer_account" "computer" {
+    
+    name = var.name
+    policy_id = data.onefuse_ad_policy.policy.id
+    workspace_url = var.workspace_url
+    template_properties = var.template_properties
+}
+
+// Output Result for AD OU Placement
+output "ad_ou" {
+  value = onefuse_microsoft_ad_computer_account.computer.final_ou
+}
