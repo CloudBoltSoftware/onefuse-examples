9fae218f7189004228fd46c05dc6a75893e6ea9c
Sid Smith <ssmith@sovlabs.com>
2021-03-02 21:23:01 -0500

    cleaned up examples

--- Terraform/modules/vsphere/folder/main.tf
+++
@@ -1,9 +0,0 @@
-data "vsphere_datacenter" "datacenter" {
-      name = "SovLabs"
-}
-
-resource "vsphere_folder" "folder" {
-      path = var.path
-      type = "vm"
-      datacenter_id = data.vsphere_datacenter.datacenter.id
-}
--- Terraform/modules/vsphere/folder/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "path" {
-  type = string
-}
