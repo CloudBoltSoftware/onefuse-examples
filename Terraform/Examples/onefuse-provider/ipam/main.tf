e8e62d3c1f34598540c859cd947e99b2bb9b024e
Sid Smith <ssmith@sovlabs.com>
2021-03-11 10:18:14 -0500

    cleaned up test files

--- Terraform/Examples/onefuse-provider/ipam/main.tf
+++ Terraform/Examples/onefuse-provider/ipam/main.tf
@@ -53,7 +53,7 @@ output "network" {
 }
 
 output "subnet" {
-  value = onefuse_ipam_record.ipam-record.network
+  value = onefuse_ipam_record.ipam-record.subnet
 }
 
 output "primary_dns" {
---
+++ Terraform/modules/onefuse/ptk/render_sps/provider.tf
@@ -0,0 +1,8 @@
+ terraform {
+  required_providers {
+    onefuse = {
+      source  = "CloudBoltSoftware/onefuse"
+   }
+  }
+  required_version = ">= 0.13"
+ }
