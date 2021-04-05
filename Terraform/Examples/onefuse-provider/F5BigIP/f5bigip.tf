3f3e31752fa09ca644d8ca2a1b7ef7dd05c2894e
Sid Smith <ssmith@sovlabs.com>
2021-04-05 11:01:33 -0400

    Adding F5 Example

--- Terraform/Examples/onefuse-provider/F5BigIP/f5bigip.tf
+++ Terraform/Examples/onefuse-provider/F5BigIP/f5bigip.tf
@@ -4,20 +4,12 @@ module "f5vip" {
     template_properties = var.template_properties
 }
 
-resource "bigip_ltm_node" "node" {
-  name             = local.node
-  address          = onefuse_ipam_record.ipam-record.ip_address
-  connection_limit = local.node_connection_limit
-  description      = "Test-Node"
-  rate_limit       = local.node_rate_limit
-  fqdn {
-    address_family = local.node_address_family
-    interval       = "3000"
-  }
+module "f5node" {
+    source = "github.com/CloudBoltSoftware/onefuse-examples.git/Terraform/modules/F5/Node"
+    f5sps = var.f5sps
+    name = local.node
+    description  = "Test Module Deploy"
+    address = onefuse_ipam_record.ipam-record.ip_address
+    pool = module.f5vip.pool_name
 }
 
-resource "bigip_ltm_pool_attachment" "attach_node" {
-  pool = module.f5vip.pool_name
-  node = format("%s:%s", bigip_ltm_node.node.name, local.node_port)
-  ratio = local.node_ratio
-}
--- Terraform/Examples/onefuse-provider/F5BigIP/properties.tf
+++ Terraform/Examples/onefuse-provider/F5BigIP/properties.tf
@@ -7,15 +7,5 @@ locals  {
 partition = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_general.partition
 
 node = format("/%s/%s", local.partition, onefuse_naming.machine.name)
-monitors = format("/%s/%s", local.partition, jsondecode(data.onefuse_static_property_set.property_set.raw).f5_pool.monitors)
-allow_nat = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_pool.allow_nat
-allow_snat = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_pool.allow_snat
-load_balancing_mode = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_pool.load_balancing_mode
-
-node_connection_limit = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_node.connection_limit
-node_rate_limit = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_node.rate_limit
-node_port = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_node.port
-node_address_family = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_node.address_family
-node_ratio = jsondecode(data.onefuse_static_property_set.property_set.raw).f5_node.ratio
 
 }
