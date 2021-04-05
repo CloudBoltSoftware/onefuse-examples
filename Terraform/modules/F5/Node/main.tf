6ec905f4a71372c886f28a22b2cc879152e3686b
Sid Smith <ssmith@sovlabs.com>
2021-04-05 13:54:28 -0400

    Adding F5 Example

--- Terraform/modules/F5/Node/main.tf
+++ Terraform/modules/F5/Node/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    onefuse = {
+      source  = "CloudBoltSoftware/onefuse"
+      version = ">= 1.20.0"
+   }
    bigip = {
       source = "F5Networks/bigip"
       version = "1.8.0"
