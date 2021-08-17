ebbe6cf9c07aae841ed6e010981d4d127f8a1ba9
sidsovlabs <66967367+sidsovlabs@users.noreply.github.com>
2021-08-17 16:04:34 -0400

    Update main.tf

--- Terraform/Examples/onefuse-provider/ipam/main.tf
+++ Terraform/Examples/onefuse-provider/ipam/main.tf
@@ -2,14 +2,12 @@
   required_providers {
     onefuse = {
       source  = "CloudBoltSoftware/onefuse"
-      version = ">= 1.20.0"
+      version = ">= 1.30.0"
    }
   }
   required_version = ">= 0.13"
 }
-
 provider "onefuse" {
-
   scheme     = var.onefuse_scheme
   address    = var.onefuse_address
   port       = var.onefuse_port
@@ -17,42 +15,33 @@ provider "onefuse" {
   password   = var.onefuse_password
   verify_ssl = var.onefuse_verify_ssl
 }
-
 data "onefuse_ipam_policy" "policy" {
   name = var.policy
 }
-
 resource "onefuse_ipam_record" "ipam-record" {
     
     hostname = var.hostname
     policy_id = data.onefuse_ipam_policy.policy.id
     template_properties = var.template_properties
 }
-
 output "ip_address" {
   value = onefuse_ipam_record.ipam-record.ip_address
 }
-
 output "netmask" {
   value = onefuse_ipam_record.ipam-record.netmask
 }
-
 output "gateway" {
   value = onefuse_ipam_record.ipam-record.gateway
 }
-
 output "network" {
   value = onefuse_ipam_record.ipam-record.network
 }
-
 output "subnet" {
   value = onefuse_ipam_record.ipam-record.subnet
 }
-
 output "primary_dns" {
   value = onefuse_ipam_record.ipam-record.primary_dns
 }
-
 output "secondary_dns" {
   value = onefuse_ipam_record.ipam-record.secondary_dns
 }
