aaf544613dfce14ed3b91aa6368baa54dbd68231
sidsovlabs <66967367+sidsovlabs@users.noreply.github.com>
2021-08-17 16:01:30 -0400

    Update main.tf

--- Terraform/Examples/onefuse-provider/naming/main.tf
+++ Terraform/Examples/onefuse-provider/naming/main.tf
@@ -7,9 +7,7 @@
   }
   required_version = ">= 0.13"
 }
-
 provider "onefuse" {
-
   scheme     = var.onefuse_scheme
   address    = var.onefuse_address
   port       = var.onefuse_port
@@ -17,16 +15,13 @@ provider "onefuse" {
   password   = var.onefuse_password
   verify_ssl = var.onefuse_verify_ssl
 }
-
 data "onefuse_naming_policy" "policy" {
   name = var.policy
 }
-
 resource "onefuse_naming" "machine" {
   naming_policy_id        = data.onefuse_naming_policy.policy.id
   template_properties     = var.template_properties
 }
-
 output "hostname" {
   value = onefuse_naming.machine.name
 }
